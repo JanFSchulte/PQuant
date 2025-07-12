a1ccfdd74e6a4bd68720f42691514ade6745a596
Arghya112358 <arghyaranjandas@gmail.com>
2025-07-12 05:07:29 +0200

    regular push

--- src/smartpixels/models/conv2D.py
+++ src/smartpixels/models/conv2D.py
@@ -18,25 +18,29 @@ def var_network(var, hidden, output):
         kernel_regularizer=tf.keras.regularizers.L1L2(0.01),
     )(var)
 
-def conv_network(var, n_filters, kernel_size):
+def conv_network(var, n_filters, 
+                 SepConv2D_kernel_size, Conv2D_kernel_size):
     var = SeparableConv2D(
-        n_filters,kernel_size,
+        n_filters,SepConv2D_kernel_size,
         depthwise_regularizer=tf.keras.regularizers.L1L2(0.01),
         pointwise_regularizer=tf.keras.regularizers.L1L2(0.01),
         activity_regularizer=tf.keras.regularizers.L2(0.01)
         )(var)
     var = Activation("tanh")(var)
     var = Conv2D(
-        n_filters,1,
+        n_filters,Conv2D_kernel_size,
         kernel_regularizer=tf.keras.regularizers.L1L2(0.01),
         activity_regularizer=tf.keras.regularizers.L2(0.01)
         )(var)
     var = Activation("tanh")(var)    
     return var
 
-def CreateModel(shape, n_filters, kernel_size, pool_size, hidden, output):
+def CreateModel(shape, n_filters, SepConv2D_kernel_size, 
+                Conv2D_kernel_size, pool_size, hidden, output):
     x_base = x_in = Input(shape)
-    stack = conv_network(x_base,  n_filters, kernel_size)
+    stack = conv_network(x_base,  n_filters, 
+                         SepConv2D_kernel_size, 
+                         Conv2D_kernel_size)
     stack = AveragePooling2D(
         pool_size=(pool_size, pool_size), 
         strides=None, 
@@ -44,6 +48,6 @@ def CreateModel(shape, n_filters, kernel_size, pool_size, hidden, output):
         data_format=None,        
     )(stack)
     stack = Activation("tanh")(stack)
-    stack = var_network(stack, hidden=16, output=output)
+    stack = var_network(stack, hidden=hidden, output=output)
     model = Model(inputs=x_in, outputs=stack)
     return model
