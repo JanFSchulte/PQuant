2626117c42790e1cd43fdca08459598ec4d288f5
Arghya112358 <arghyaranjandas@gmail.com>
2025-08-03 00:05:45 -0400

    Regular Push

--- src/pquant/pruning_methods/mdmm.py
+++ src/pquant/pruning_methods/mdmm.py
@@ -424,6 +424,8 @@ class PACAPatternMetric:
         return self.projection_mask
 
     def _get_projection_mask(self, weight):
+        if len(ops.shape(weight)) != 4:
+            return ops.ones_like(weight)
         _, _, (C_out, C_in, kH, kW) = self._get_kernels_and_patterns(weight, self.src)
         _, distances = self._pattern_distances(weight) # Shape: (C_out*C_in, num_dominant)
         closest_pattern_indices = ops.argmin(distances, axis=1) # Shape: (C_out*C_in,)
