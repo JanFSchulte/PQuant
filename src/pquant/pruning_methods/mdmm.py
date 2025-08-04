eb5132a6bc53882e412d4f0d9ed69a55aa273265
Arghya112358 <arghyaranjandas@gmail.com>
2025-08-04 00:36:36 -0400

    Regular Push

--- src/pquant/pruning_methods/mdmm.py
+++ src/pquant/pruning_methods/mdmm.py
@@ -440,9 +440,9 @@ class PACAPatternMetric:
             raise ValueError("Projection mask has not been selected yet.")
         
         projection_mask = self.projection_mask
-        projected_weight = (weight + 2*self.epsilon) * projection_mask
-        _, all_patterns, _ = self._get_kernels_and_patterns(projected_weight, 
-                                                            self.src, epsilon=self.epsilon)
+        # projected_weight = (weight + 2*self.epsilon) * projection_mask
+        _, all_patterns, _ = self._get_kernels_and_patterns(projection_mask, 
+                                                            self.src, epsilon=0.0)
         unique_patterns, counts = self._get_unique_patterns_with_counts(all_patterns)
         
         return unique_patterns, counts
